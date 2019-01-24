daab1c114e4f7c18f9ed8a416ff1488c558c8a88
jeanettearteaga <jay8chuy@gmail.com>
2019-01-23 23:40:12 -0700

    Spaced out items in add recipe activity

--- app/src/main/java/com/arteaga/jeanette/recipeplanner/MainActivity.java
+++ app/src/main/java/com/arteaga/jeanette/recipeplanner/MainActivity.java
@@ -1,7 +1,10 @@
 package com.arteaga.jeanette.recipeplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addRecipeBtn = (Button)findViewById(R.id.open_activity_button);
+        addRecipeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewRecipeActivity.class));
+            }
+        });
     }
+
+
+
 }
